ab4210cabb30936d7d9fee175610cf29220ebea1
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-16 17:11:44 +0800

    Initial Commit

---
+++ MapApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapApp
+//
+//  Created by Zachary on 16/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
