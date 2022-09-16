09880783bb82ea55b949e730f1b879951751513c
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-09-16 17:33:57 +0800

    added location model, sample data arrays of location, and images of location

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Zachary on 16/9/22.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
